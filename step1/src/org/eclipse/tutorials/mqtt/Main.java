4df2437da5e622336df42680ed5fb3622e327ed8
Benjamin CABE <benjamin@eclipse.org>
2014-03-13 12:20:11 +0100

    license headers code formatting

--- step1/src/org/eclipse/tutorials/mqtt/Main.java
+++ step1/src/org/eclipse/tutorials/mqtt/Main.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014, Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Benjamin Cabé - initial API and implementation
+ *     Julien Vermillard - improvements
+ *******************************************************************************/
 package org.eclipse.tutorials.mqtt;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -9,42 +20,45 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try {
+		try {
 
-            final MqttClient mqttClient = new MqttClient("tcp://iot.eclipse.org:1883", MqttClient.generateClientId(),
-                    new MemoryPersistence());
+			final MqttClient mqttClient = new MqttClient(
+					"tcp://iot.eclipse.org:1883",
+					MqttClient.generateClientId(), new MemoryPersistence());
 
-            mqttClient.setCallback(new MqttCallback() {
+			mqttClient.setCallback(new MqttCallback() {
 
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
+				@Override
+				public void messageArrived(String topic, MqttMessage message)
+						throws Exception {
 
-                    try {
-                        System.out.println(topic + " => " + new String(message.getPayload()));
+					try {
+						System.out.println(topic + " => "
+								+ new String(message.getPayload()));
 
-                    } catch (RuntimeException e) {
-                        e.printStackTrace();
-                    }
-                }
+					} catch (RuntimeException e) {
+						e.printStackTrace();
+					}
+				}
 
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    // not used
-                }
+				@Override
+				public void deliveryComplete(IMqttDeliveryToken token) {
+					// not used
+				}
 
-                @Override
-                public void connectionLost(Throwable cause) {
-                    System.out.println("Connection lost: " + cause.getLocalizedMessage());
-                }
-            });
-            mqttClient.connect();
-            mqttClient.subscribe("/fosdem/#");
+				@Override
+				public void connectionLost(Throwable cause) {
+					System.out.println("Connection lost: "
+							+ cause.getLocalizedMessage());
+				}
+			});
+			mqttClient.connect();
+			mqttClient.subscribe("/fosdem/#");
 
-        } catch (MqttException e) {
-
-            e.printStackTrace();
-        }
-    }
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- step2/src/org/eclipse/tutorials/mqtt/Main.java
+++ step2/src/org/eclipse/tutorials/mqtt/Main.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014, Sierra Wireless and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Julien Vermillard - initial API and implementation
+ *     Benjamin Cabé - improvements
+ *******************************************************************************/
 package org.eclipse.tutorials.mqtt;
 
 import java.util.Calendar;
